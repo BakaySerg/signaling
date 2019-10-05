b00d7a68d345ab251df1df33b6f08f6c82ee9d22
Сергей <bakay.sergey28@gmail.com>
2019-10-05 18:06:12 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ $(document).ready(function(){
   "use strict";
 
   //mask
-	$("[type=tel]").mask("+7 (999) 999-99-99");
+  $("[type=tel]").mask("+7 (999) 999-99-99");
+
+  //form
+  $(".form").submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php',
+      data: $(this).serialize(),
+      success: function(){
+        $.magnificPopup.open({items: {src: '#popup-success'},type: 'inline',mainClass: 'mfp-fade',removalDelay: 355},0);
+        setTimeout(function(){$.magnificPopup.close()},4000);
+      },
+      error: function() {
+        $.magnificPopup.open({items: {src: '#popup-error'},type: 'inline',mainClass: 'mfp-fade',removalDelay: 355},0);
+        setTimeout(function(){$.magnificPopup.close()},4000);
+      }
+    }).done(function() {$(".form").trigger("reset");});
+    return false;
+  });
 
 });
